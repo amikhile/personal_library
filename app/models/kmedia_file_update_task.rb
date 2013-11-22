f91e6a6ea616d75fbc03630b8fd1b53fdb7e39f3
amikhile <annamik@gmail.com>
2013-11-22 12:36:03 +0100

    improving file update task

--- app/models/kmedia_file_update_task.rb
+++ app/models/kmedia_file_update_task.rb
@@ -1,26 +1,31 @@
 class KmediaFileUpdateTask
-require_relative '../../config/environment'
+  require_relative '../../config/environment'
 
 #updates kmedia file with it's file type
 
   def self.update
     @token = KmediaToken.get_token
 
-     KmediaFile.find_each do |kmedia_file|
-       response = RestClient.post "#{APP_CONFIG['kmedia_url']}/admin/api/api/files.json",
-                                  auth_token: @token,
-                                  ids: kmedia_file.kmedia_id,
-                                  secure: '0'
-       hash = JSON.parse response
-       files = hash['item']
-       file = files.first
-       if(file.present?)
-         puts kmedia_file.name
-         kmedia_file.ext = file['type']
-         kmedia_file.save
-         puts kmedia_file.ext
-       end
-     end
+    KmediaFile.find_each do |kmedia_file|
+      unless kmedia_file.ext.present?
+        response = RestClient.post "#{APP_CONFIG['kmedia_url']}/admin/api/api/files.json",
+                                   auth_token: @token,
+                                   ids: kmedia_file.kmedia_id,
+                                   secure: '0'
+        hash = JSON.parse response
+        files = hash['item']
+        file = files.first
+        if (file.present?)
+          puts kmedia_file.name
+
+          kmedia_file.ext = file['type']
+          kmedia_file.save
+          puts kmedia_file.ext
+        end
+      else
+        puts  "#{kmedia_file.name} -  #{kmedia_file.ext}"
+      end
+    end
   end
 
 end
