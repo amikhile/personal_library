78f1c733430b5e6031478d3c842ab23b23369b4a
amikhile <annamik@gmail.com>
2013-12-23 15:01:53 +0100

    Adding translation to user reports

--- lib/user_report.rb
+++ lib/user_report.rb
@@ -5,7 +5,7 @@ class UserReport
   def  self.reports_to_csv (reports)
 
     CSV.generate do |csv|
-      column_names = ['Username','Email','Registered at','Login_last_week','Login_last_month','Total logins']
+      column_names = ['Username','Email','Registered at','Last week','Last month','Total logins']
       csv << column_names
       reports.each do |report|
         csv << ["#{report.user.username}","#{report.user.email}","#{report.user.created_at.to_date}","#{report.logins_count}","#{report.login_last_week}","#{report.login_last_month}"]
