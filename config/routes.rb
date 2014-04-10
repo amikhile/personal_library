6798c3598414e52506182e8087e3ec2a9597ad49
amikhile <annamik@gmail.com>
2014-04-10 13:25:20 +0200

    temporary closing my_files api until ssl enabled

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,7 @@ PersonalLibrary::Application.routes.draw do
     resources :api, only: [] do
       collection do
         get :morning_lessons
-        get :my_files
+#        get :my_files
       end
 
     end
